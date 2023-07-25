a67c77140109b225fcc3b64b23e1a668daa9da46
Julian Khan <juliankhan.b@gmail.com>
2023-07-25 17:25:58 +0800

    Create constructor and getFirstName method

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int employeeId;
+
+    public Employee(String firstName, String lastName, int employeeId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,3 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EmployeeTest {
-    
+
+    private Employee employeeJohn = new Employee("John", "Doe", 12435);
+
+    @Test
+    @DisplayName("Check that the Employee object's getFirstName method works")
+    public void testGetFirstName() {
+        assertTrue(employeeJohn.getFirstName() == "John");
+    } 
+
+
+
 }
