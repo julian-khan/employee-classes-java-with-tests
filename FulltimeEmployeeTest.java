5434c872f4de3f138518a463c9ace842cef46846
Julian Khan <juliankhan.b@gmail.com>
2023-07-25 18:00:37 +0800

    Create inheritance unit tests

---
+++ FulltimeEmployeeTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FulltimeEmployeeTest {
+
+    private FullTimeEmployee bob
+
+     = new Employee("Bob", "Kent", 41234);
+
+    @Test
+    @DisplayName("Check that the Employee getFirstName method works")
+    public void testGetFirstName() {
+        assertTrue(bob
+
+        .getFirstName() == "Bob");
+    }
+    
+    @Test
+    @DisplayName("Check that the Employee getLastName method works")
+    public void testGetLastName() {
+        assertTrue(bob
+
+        .getLastName() == "Kent");
+    }
+
+    @Test
+    @DisplayName("Check that the employee's ID can be retrieved")
+    public void testGetEmployeeId() {
+        assertTrue(bob
+
+        .getEmployeeId() == 12435);
+    }
+
+    @Test
+    @DisplayName("Check that the default constructor can be used")
+    public void testUseDefaultConstructor() {
+        Employee todd = new Employee();
+        assertAll(
+                "Check that the private instance variables are properly instantiated", 
+                () -> assertTrue( todd.getFirstName() == ""),
+                () -> assertTrue(todd.getLastName() == ""),
+                () -> assertTrue(todd.getEmployeeId() == 0)
+                );
+    }
+}
